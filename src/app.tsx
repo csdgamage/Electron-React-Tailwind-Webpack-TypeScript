a32c55ceb63536849c7d61c1789894c5eb1b58a0
Charith Gamage <charith.sankalpa@hotmail.com>
2023-08-06 10:13:48 +0530

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './app.css'
+import Home from './pages/Home'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
